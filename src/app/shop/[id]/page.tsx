5dcb1dd0a60d9ad359829fa3d9e9d5a843a1b87b
rootspyro <root.spyro@gmail.com>
2024-03-31 03:21:03 -0400

    Feat: Add Home page for propose

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -88,7 +88,7 @@ export default function Product() {
           </div>
           <div className="flex flex-row gap-4 w-full">
             <input onChange={ e => SetQuantity(Number(e.target.value))} className="max-w-16 px-2 py-2 text-sm text-footer border rounded" type="number" value={product.quantity} min="1"></input>
-            <button onClick={AddToCar} className={`text-sm text-white bg-button rounded py-2 px-5 align-center transition-all duration-200 hover:shadow-xl w-full max-w-48`}>Agregar al carrito</button>
+            <button onClick={AddToCar} className={`text-sm text-white bg-button rounded-sm hover:bg-amber-600 py-2 px-5 align-center transition-all duration-200 hover:shadow-xl w-full max-w-48`}>Agregar al carrito</button>
           </div>
 
       </div>
