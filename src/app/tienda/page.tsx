5162da2a02690c72383933f35bdb879998b27454
rootspyro <root.spyro@gmail.com>
2024-04-02 15:50:41 -0400

    Feat: modify shop page

--- src/app/tienda/page.tsx
+++ src/app/tienda/page.tsx
@@ -14,9 +14,8 @@ export default function Shop() {
     <Navbar/>
     <div className="w-full flex flex-col p-10 my-16">
       <h2 className="text-3xl w-full text-center font-bold sm:ml-10 text-center sm:text-left text-footer">Nuestros productos</h2>
-      <div className="flex sm:flex-row flex-col gap-14 justify-center items-center align-center mt-16">
+      <div className="flex sm:flex-row flex-col gap-14 justify-center items-center md:h-80 mt-16">
       { Products.map((product: Product) => {
-        
         return <Link href={`/tienda/${product.id}`}> <ProductCard key={product.id} product={product} /></Link>
       })}
       </div>
